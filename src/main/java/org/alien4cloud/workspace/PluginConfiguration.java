75e393c3c13a942bb0aeda2a5d26db10578c7419
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2016-10-05 17:33:38 +0200

    ALIEN-1795 : Plugin endpoints are not secured even if annotated with @PreAuthorize

--- src/main/java/org/alien4cloud/workspace/PluginConfiguration.java
+++ src/main/java/org/alien4cloud/workspace/PluginConfiguration.java
@@ -1,8 +1,13 @@
 package org.alien4cloud.workspace;
 
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
 @Configuration
 @ComponentScan({ "org.alien4cloud.workspace" })
 public class PluginConfiguration {
