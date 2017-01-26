f60f1aa2eebd12673947a16d85637c1e34de8e99
Luc Boutier <luc@fastconnect.fr>
2017-01-26 17:32:27 +0100

    [ALIEN-2026] Events renamed for consistency

--- src/main/java/org/alien4cloud/workspace/listener/ApplicationDeletionListener.java
+++ src/main/java/org/alien4cloud/workspace/listener/ApplicationDeletionListener.java
@@ -8,16 +8,16 @@ import org.alien4cloud.workspace.service.WorkspaceService;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import alien4cloud.events.BeforeApplicationDeletedEvent;
+import org.alien4cloud.alm.events.BeforeApplicationDeleted;
 
 @Component
-public class ApplicationDeletionListener implements ApplicationListener<BeforeApplicationDeletedEvent> {
+public class ApplicationDeletionListener implements ApplicationListener<BeforeApplicationDeleted> {
 
     @Resource
     private WorkspaceService workspaceService;
 
     @Override
-    public void onApplicationEvent(BeforeApplicationDeletedEvent event) {
+    public void onApplicationEvent(BeforeApplicationDeleted event) {
         workspaceService.deleteWorkspace(new Workspace(Scope.APPLICATION, event.getApplicationId(), null).getId());
     }
 }
